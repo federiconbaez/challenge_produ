3912cc6f5be3c109c1d2fb06ebe4f608c2c3500c
Federico Nicolas Báez <federiconbaez@gmail.com>
2023-05-30 13:14:35 -0300

    feat: Refactor elements into view, and styles

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ const books = [
       imageUrl: "book1.jpg"
     },
     {
-        title: "Book 2",
-        author: "Author 2",
-        summary: "Summary of Book 2",
-        rating: 4.5,
-        genre: "Fiction",
-        language: "English",
-        publicationDate: "2022-01-01",
-        imageUrl: "book2.jpg"
+      title: "Book 2",
+      author: "Author 2",
+      summary: "Summary of Book 2",
+      rating: 4.5,
+      genre: "Fiction",
+      language: "English",
+      publicationDate: "2022-01-01",
+      imageUrl: "book2.jpg"
     }
-];
+  ];
   
-// Function to render book cards
-function renderBooks(books) {
+  // Function to render book cards
+  function renderBooks(books) {
     const booksGrid = document.getElementById("books-grid");
     booksGrid.innerHTML = "";
   
@@ -45,54 +45,49 @@ function renderBooks(books) {
   
       const bookSummary = document.createElement("p");
       bookSummary.textContent = book.summary;
-      bookCard.appendChild(bookSummary);
-  
-      // Add more book details here...  
+      bookCard.appendChild(bookSummary);  
       booksGrid.appendChild(bookCard);
     });
   }
   
-  // Event listener for search button click
-  const searchButton = document.getElementById("search-button");
-  searchButton.addEventListener("click", () => {
-    const searchInput = document.getElementById("search-input").value;
-    // Perform search based on the input value and update the books array with the search results
-    const searchResults = books.filter(book =>
-      book.title.toLowerCase().includes(searchInput.toLowerCase()) ||
-      book.author.toLowerCase().includes(searchInput.toLowerCase()) ||
-      book.summary.toLowerCase().includes(searchInput.toLowerCase())
-    );
-    renderBooks(searchResults);
-  });
-
-// Function to filter books based on selected options
-function filterBooks() {
+  // Function to filter books based on selected options
+  function filterBooks() {
     const genreFilter = document.getElementById("genre-select").value;
     const languageFilter = document.getElementById("language-select").value;
     const ratingFilter = document.getElementById("rating-select").value;
   
-    let filteredBooks = books;
-  
-    if (genreFilter) {
-      filteredBooks = filteredBooks.filter(book => book.genre === genreFilter);
-    }
-  
-    if (languageFilter) {
-      filteredBooks = filteredBooks.filter(book => book.language === languageFilter);
-    }
-  
-    if (ratingFilter) {
-      filteredBooks = filteredBooks.filter(book => book.rating >= ratingFilter);
-    }
+    const filteredBooks = books.filter(book => {
+      if (genreFilter && book.genre !== genreFilter) {
+        return false;
+      }
+      if (languageFilter && book.language !== languageFilter) {
+        return false;
+      }
+      if (ratingFilter && book.rating < ratingFilter) {
+        return false;
+      }
+      return true;
+    });
   
     renderBooks(filteredBooks);
-}
-
-// Event listeners for filter options change
-document.getElementById("genre-select").addEventListener("change", filterBooks);
-document.getElementById("language-select").addEventListener("change", filterBooks);
-document.getElementById("rating-select").addEventListener("change", filterBooks);
-
-// Initial rendering of books
-renderBooks(books);
+  }
+  
+  // Event listeners
+  document.getElementById("search-button").addEventListener("click", () => {
+    const searchInput = document.getElementById("search-input").value.toLowerCase();
+    const searchResults = books.filter(
+      book =>
+        book.title.toLowerCase().includes(searchInput) ||
+        book.author.toLowerCase().includes(searchInput) ||
+        book.summary.toLowerCase().includes(searchInput)
+    );
+    renderBooks(searchResults);
+  });
+  
+  document.getElementById("genre-select").addEventListener("change", filterBooks);
+  document.getElementById("language-select").addEventListener("change", filterBooks);
+  document.getElementById("rating-select").addEventListener("change", filterBooks);
+  
+  // Initial rendering of books
+  renderBooks(books);
   
